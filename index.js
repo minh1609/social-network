cc443ea8aa6288d98f31c87c71373b81892189b8
minh997 <39399443+minh997@users.noreply.github.com>
2019-06-13 23:51:17 -0700

    deployment

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ connectDB();
 app.use(bodyParser.json());
 
 //Define Route
+
 require("./routes/stream")(app);
+
+if (process.env.NODE_ENV === "production") {
+    //If route do not match express round, try to match it with React Router
+    app.use(express.static("client/build"));
+    const path = require("path");
+
+    //If route is not defined, send back HTML file
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {

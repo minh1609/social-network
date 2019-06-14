380ba53dce81c78c13c03fd2c0f89ce0cfdfebb2
minh997 <39399443+minh997@users.noreply.github.com>
2019-06-14 04:00:59 -0700

    fixed

--- db.js
+++ db.js
@@ -4,7 +4,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URL, { useNewUrlParser: true });
+        await mongoose.connect(
+            process.env.DB_URL ||
+                "mongodb+srv://ADMIN-MINH:mM160997%40@cluster0-efrdl.mongodb.net/main",
+            {
+                useNewUrlParser: true
+            }
+        );
         console.log("database connected");
     } catch (error) {
         console.log(error.message);

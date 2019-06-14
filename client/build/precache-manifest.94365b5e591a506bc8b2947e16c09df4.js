9628f82065df7be1aa0562767d776a43bf66073f
minh997 <39399443+minh997@users.noreply.github.com>
2019-06-14 00:02:30 -0700

    deploy

---
+++ client/build/precache-manifest.94365b5e591a506bc8b2947e16c09df4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ef1311af23bbb17e8596230b938eb01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dccc8cf5e0faef62faa1",
+    "url": "/static/js/2.24a82f31.chunk.js"
+  },
+  {
+    "revision": "bd9910980db6e5b39966",
+    "url": "/static/js/main.bd762a0a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
